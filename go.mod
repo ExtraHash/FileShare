84d111f10b53596e59e5fad646607d9c08caa9cf
ExtraHash <ExtraHash@protonmail.com>
2020-11-18 22:21:10 -0700

    listen

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/ExtraHash/FileShare
 
 go 1.15
 
-require github.com/ExtraHash/p2p v0.0.0-20201119033119-4a4e6c3bde11
+require github.com/ExtraHash/p2p v0.0.0-20201119051956-ddb12d37e643
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/hex"
 	"flag"
+	"fmt"
 	"time"
 
 	"github.com/ExtraHash/p2p"
@@ -26,15 +28,27 @@ func main() {
 
 	p2p := p2p.DP2P{}
 	go p2p.Initialize(config)
+	go listen(&p2p)
+	chatter(&p2p)
+}
 
+func chatter(p2p *p2p.DP2P) {
 	for {
 		time.Sleep(5 * time.Second)
 		p2p.Broadcast(randomData())
 	}
 }
 
+func listen(p2p *p2p.DP2P) {
+	for {
+		message := p2p.ReadMessage()
+		fmt.Println("received", string(message))
+	}
+}
+
 func randomData() []byte {
 	token := make([]byte, 32)
 	rand.Read(token)
-	return token
+	// return token
+	return []byte(hex.EncodeToString(token))
 }
